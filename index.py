fe76e5f8ee716a2f02ec355c6a0c271dc9f6df87
Preston <yaumanyoung@yahoo.com>
2022-02-09 12:21:14 -0800

    Ignored DEV

---
+++ index.py
@@ -0,0 +1,37 @@
+# {
+#   "token": {
+#       "token_frequency": frequency (int)
+#       "tf_idf_score": score (int)
+#       "doc_ids": {            # can implicitly derive from len(doc_ids)
+#           "id1": weight (int) 
+#       }
+#       "doc_ids": [
+#           {
+#               "id": page id (string?),
+#               "weight": weight (int)
+#           },
+#           {
+#               "id": page id (string?),
+#               "weight": weight (int)
+#           }
+#       ]
+#   }
+# }
+#
+#
+# 
+
+index_dump = 'dump.json'
+doc_count = 0
+doc_id = 
+
+
+
+def parse_json():
+
+    for file in os.listdir(os.path.dirname(...))    # root here?
+
+
+
+
+    return
