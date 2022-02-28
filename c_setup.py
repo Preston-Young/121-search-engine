aa05e8747de2e288b4d6dc139437534d5b0d64ce
Zubair Sidhu <sidhuzubair@gmail.com>
2022-02-28 13:00:51 -0800

    Start testing dictionary sorting

--- c_setup.py
+++ c_setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 from Cython.Build import cythonize
+import numpy
 
 # Build using: python c_setup.py build_ext --inplace
 
@@ -7,4 +8,5 @@ setup(
     name='Top result helper',
     ext_modules=cythonize("top_results_helper.pyx", language_level="3"),
     zip_safe=False,
+    include_dirs=[numpy.get_include()]
 )
--- c_testing.py
+++ c_testing.py
@@ -2,12 +2,28 @@ from time import time
 from nltk.stem import PorterStemmer
 
 from term_loading import load_index
-from top_results_helper import get_top_results
+from top_results_helper import get_top_results, score_results
 
 # Removed from git
-# from common import common_doc_ids
+from common import common_doc_ids
 
-if __name__ == '__main__':
+def dict_sorting_test():
+    score_size = 1000000
+    score_keys = [str(x).encode() for x in range(score_size)]
+    score_vals = [str(x).encode() for x in range(score_size)]
+    scores = dict(zip(score_keys, score_vals))
+    
+    start = time()  # start timer
+
+    res = score_results(scores, 5)
+    
+    end = time()    # end timer
+    run_time = round((end - start) * 1000, 4)
+    print(f'\nRuntime: {run_time}ms')
+    print(res)
+
+
+def top_results_test():
     query_terms = ['machine', 'learning']
     
     stemmer = PorterStemmer()
@@ -21,3 +37,7 @@ if __name__ == '__main__':
     end = time()    # end timer
     run_time = round((end - start) * 1000, 4)
     print(f'\nRuntime: {run_time}ms')
+
+if __name__ == '__main__':
+    # top_results_test()
+    dict_sorting_test()
